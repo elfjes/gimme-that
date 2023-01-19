7004413ba3af92828b058d406bff177e1027f97e
Pelle Koster <pellekoster@matgras.nl>
2023-01-19 18:32:37 +0100

    bump version

--- src/gimme/__init__.py
+++ src/gimme/__init__.py
@@ -16,7 +16,7 @@ from .repository import LayeredRepository, SimpleRepository
 from .resolvers import AttributeResolver, TypeHintingResolver
 from .types import DependencyInfo
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 __all__ = [
     "add",
